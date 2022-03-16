90e91af299bd0b3a6c55bbc2945d976ebac12fd6
Janice Chan <janice.chan1288@gmail.com>
2022-03-16 07:55:28 -0400

    commit tree d4a3bc8c90600ada8ec523d6cf091ca996860254

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2); 
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(integer) {
+    return function multiplyFare() {
+        return integer * integer;
+    }
+    }
+
+const fareDoubler = function(fare) {
+    return fare * 2;
+}
+
+const fareTripler = function(fare) {
+    return fare * 3;
+}
+
+const selectDifferentDrivers = function(arrayDrivers, returnDriversFunc) {
+    if(returnDriversFunc === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(arrayDrivers);
+    } else {
+        return returnLastTwoDrivers(arrayDrivers);
+    }
+   }
